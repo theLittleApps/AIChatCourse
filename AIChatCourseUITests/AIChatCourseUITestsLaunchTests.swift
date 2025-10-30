4a6ac4a17e797ca21d2f7fb686e352566bcaa964
018584-RD2(黃彥儒) <jimmy.huang@senao.com.tw>
2025-10-30 17:05:42 +0800

    [Feature] add SwiftLint

--- AIChatCourseUITests/AIChatCourseUITestsLaunchTests.swift
+++ AIChatCourseUITests/AIChatCourseUITestsLaunchTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class AIChatCourseUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
