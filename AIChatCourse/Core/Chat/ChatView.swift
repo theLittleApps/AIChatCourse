82c80fa343fc6ffdc4821975676bf4a371da6693
018584-RD2(黃彥儒) <jimmy.huang@senao.com.tw>
2025-11-24 17:33:22 +0800

    [Feature] Continue making ChatView

--- AIChatCourse/Core/Chat/ChatView.swift
+++ AIChatCourse/Core/Chat/ChatView.swift
@@ -12,29 +12,110 @@ struct ChatView: View {
     @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
     @State private var avatar: AvatarModel? = .mock
     @State private var currentUser: UserModel? = .mock
+    @State private var textFieldText: String = ""
+    
+    @State private var showChatSetting: Bool = false
     
     var body: some View {
         VStack(spacing: 0) {
-            ScrollView {
-                LazyVStack(spacing: 24) {
-                    ForEach(chatMessages) { message in
-                        let isCurrentUser = message.authorId == currentUser?.userId
-                        ChatBubbleViewBuilder(
-                            message: message,
-                            isCurrentUser: isCurrentUser,
-                            imageName: isCurrentUser ? nil : avatar?.profileImageName
-                        )
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding(8)
-            }
-            
-            Rectangle()
-                .frame(height: 50.0)
+            scrollViewSection
+            textFieldSection
         }
         .navigationTitle(avatar?.name ?? "Chat")
         .toolbarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Image(systemName: "ellipsis")
+                    .padding(8)
+                    .anyButton {
+                        onChatSettingPressed()
+                    }
+            }
+        }
+        .confirmationDialog("", isPresented: $showChatSetting) {
+            Button(role: .destructive) {
+                
+            } label: {
+                Text("Report User / Chat")
+            }
+            
+            Button(role: .destructive) {
+                
+            } label: {
+                Text("Delete Chat")
+            }
+        } message: {
+            Text("What would you like to do?")
+        }
+    }
+    
+    private var scrollViewSection: some View {
+        ScrollView {
+            LazyVStack(spacing: 24) {
+                ForEach(chatMessages) { message in
+                    let isCurrentUser = message.authorId == currentUser?.userId
+                    ChatBubbleViewBuilder(
+                        message: message,
+                        isCurrentUser: isCurrentUser,
+                        imageName: isCurrentUser ? nil : avatar?.profileImageName
+                    )
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(8)
+        }
+    }
+    
+    private var textFieldSection: some View {
+        TextField("Say somthing...", text: $textFieldText)
+            .keyboardType(.alphabet)
+            .autocorrectionDisabled()
+            .padding(12)
+            .padding(.trailing, 60)
+            .overlay(alignment: .trailing, content: {
+                Image(systemName: "arrow.up.circle.fill")
+                    .font(.system(size: 32))
+                    .padding(.trailing, 4)
+                    .foregroundStyle(.accent)
+                    .anyButton(.plain) {
+                        onSendMessagePressed()
+                    }
+            })
+            .background(
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(Color(uiColor: .systemBackground))
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color.gray.opacity(0.03), lineWidth: 1)
+                }
+            )
+            .padding(.horizontal, 12)
+            .padding(.vertical, 6)
+            .background(Color(uiColor: .secondarySystemBackground))
+    }
+    
+    private func onSendMessagePressed() {
+        guard let currentUser else { return }
+        
+        let content = textFieldText
+        
+        let message = ChatMessageModel(
+            id: UUID().uuidString,
+            chatId: UUID().uuidString,
+            authorId: currentUser.userId,
+            content: content,
+            seenByIds: nil,
+            dateCreated: .now
+        )
+        
+        chatMessages.append(message)
+        
+        textFieldText = ""
+    }
+    
+    private func onChatSettingPressed() {
+        showChatSetting = true
     }
 }
 
